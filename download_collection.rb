72b04588c9fc196793dcc7637506967484d6b44f
Marcus Bauer <mabako@gmail.com>
2016-02-25 19:00:47 +0100

    tweaks to puzzle downloading

--- download_collection.rb
+++ download_collection.rb
@@ -2,13 +2,15 @@ require_relative 'puzzle'
 
 raise ArgumentError, 'Missing ID of the puzzle to check' if ARGV.length == 0
 collection_id = ARGV[0].to_i
+folder_name = ARGV[1].nil? ? collection_id.to_s : ARGV[1]
 raise ArgumentError, 'not a numeric puzzle id' if collection_id.nil?
 
-Dir.mkdir(collection_id.to_s) unless Dir.exist?(collection_id.to_s)
-Dir.chdir(collection_id.to_s)
+Dir.mkdir(folder_name) unless Dir.exist?(folder_name)
+Dir.chdir(folder_name)
 
 puzzles = Puzzle.collection_summary(collection_id)
+len = (puzzles.size + 1).to_s.length
 puzzles.each_with_index { |puzzle, index|
-  Puzzle.from_url(puzzle['id']).save("#{index+1}.sgf")
+  Puzzle.from_url(puzzle['id']).save(sprintf("%0#{len}d.sgf", index+1))
   print '.'
 }
